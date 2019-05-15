1290e659ac15d7c3ec77e7223034c09b6d4f0a02
paula <49191579+canfanga@users.noreply.github.com>
2019-05-15 12:05:36 +0200

    Pausa feta :)
    Com me la torneu a liar us rebento <3

---
+++ Street Fighter/Scenes/Game/Extras/Pause.gd
@@ -0,0 +1,24 @@
+extends Control
+
+func _input(event):
+	 nova_pausa()
+	
+func nova_pausa(): 
+	if Input.is_action_pressed ("Pause"):
+		var new_pause_state =not get_tree().paused
+		get_tree().paused = new_pause_state
+		visible= new_pause_state
+		
+
+func _on_Resume_pressed():
+	nova_pausa()
+	visible= not visible
+	get_tree().paused = false 
+
+func _on_Title_Screen_pressed():
+	get_tree().change_scene('res://Scenes/Main Menu/Main Menu.tscn')
+	get_tree().paused = false 
+
+func _on_Options_pressed():
+	get_tree().change_scene('res://Scenes/Cepeda.tscn')
+	get_tree().paused = false 
